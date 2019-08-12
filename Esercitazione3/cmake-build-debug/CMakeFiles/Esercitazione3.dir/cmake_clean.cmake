83c9ad2563b918195d3b994674c6d696eca87cee
Rocco Luigi Scarcella <roccoluigis95@gmail.com>
2019-08-12 19:18:25 +0200

    added Esercitazione3

---
+++ Esercitazione3/cmake-build-debug/CMakeFiles/Esercitazione3.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Esercitazione3.dir/main.cpp.obj"
+  "Esercitazione3.pdb"
+  "Esercitazione3.exe"
+  "Esercitazione3.exe.manifest"
+  "libEsercitazione3.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Esercitazione3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
